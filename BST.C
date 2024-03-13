3aa29756871af6ddf8b23870ee99e283df0f5ff8
PATEL AVI <patelavi47.app@gmail.com>
2024-03-13 09:51:40 +0530

    final commit

---
+++ BST.C
@@ -0,0 +1,33 @@
+//a1qz
+#include<stdio.h>
+typedef struct node
+{
+   int info;
+   struct node *lp,*rp;
+}node;
+
+node *createBST(node *t,int number)
+{
+   if(t == NULL)
+   {
+      t=(node *)malloc(sizeof(node));
+
+   }
+   else if()
+   {
+
+   }
+   else
+   {
+
+   }
+   return t;
+}
+void main()
+{
+   node *root=NULL;
+   int number;
+   clrcsr();
+
+   getch();
+}
